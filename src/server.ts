5db82b43cb116633b7f13709cf51dea5f8f8b85b
EliezerGomes <eliezerportilho@gmail.com>
2023-05-17 09:04:34 -0300

    api liberada

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,11 @@
 import express from 'express'
 import {routes} from "./routes"
-const cors = require('cors')
+const cors = require("cors")
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use(routes)
-app.use(cors())
 
 app.listen(3000, () => console.log("Server is running"))
