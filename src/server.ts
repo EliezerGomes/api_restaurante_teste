77d8cef0475e211d1d74e9d709f2b771a2c30279
EliezerGomes <eliezerportilho@gmail.com>
2023-06-16 11:06:16 -0300

    api finalizada 3

--- src/server.ts
+++ src/server.ts
@@ -3,9 +3,10 @@ import {routes} from "./routes"
 const cors = require("cors")
 
 const app = express()
+const port = process.env.PORT || 3000
 
 app.use(cors())
 app.use(express.json())
 app.use(routes)
 
-app.listen(3000, () => console.log("Server is running"))
+app.listen(port, () => console.log("Server is running"))
